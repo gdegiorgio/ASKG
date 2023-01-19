530c0dd578bbf6d9de5357d5b141157cd20c1fe1
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2023-01-19 10:13:50 +0100

    Change static query values

--- src/kg/wikidata/WikidataEndpoint.ts
+++ src/kg/wikidata/WikidataEndpoint.ts
@@ -9,17 +9,9 @@ export class WikidataEndpoint implements SparqlEndpoint{
             let response: WikidataResponse = new WikidataResponse();
             response.data = {}
 
-
-
-            let mock_query = `SELECT ?concept ?birthdate WHERE {
-
-                ?concept rdfs:label "Elizabeth II"@en . 
-                ?concept wdt:P569 ?birthdate . 
-               
-               }`
-            console.log("Generated Wikidata Query : ", mock_query)
+            console.log("Generated Wikidata Query : ", sparql_query)
             let client = new SparqlClient({ endpointUrl: url });
-            let dataStream = await client.query.select(mock_query);
+            let dataStream = await client.query.select(sparql_query);
             let res_arr = []
             dataStream.on("data", (data: any) => {
                 res_arr.push(data)
